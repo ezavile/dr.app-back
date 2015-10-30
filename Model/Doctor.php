5da8c5e906b55cf8af0cdd9be6fbfd767f89e324
Edgar Zavala <ezavile@gmail.com>
2015-10-29 18:33:41 -0600

    Add: Doctor -> get, new

--- Model/Doctor.php
+++ Model/Doctor.php
@@ -19,24 +19,23 @@ function getDoctores() {
 function addDoctor() {
 	$request = \Slim\Slim::getInstance()->request();
 	$doc = json_decode($request->getBody());
-	$sql = "INSERT INTO doctor (usuario, password, nombre, servicios, telefono, horario, direccion, correo, foto1, foto2, foto3) VALUES (:usuario, :password, :nombre, :servicios, :telefono, :horario, :direccion, :correo, :foto1, :foto2, :foto3)";
+	$sql = "INSERT INTO doctor (usuario, password, nombre, servicios, telefono, direccion, correo, foto1, foto2, foto3) VALUES (:usuario, :password, :nombre, :servicios, :telefono, :direccion, :correo, :foto1, :foto2, :foto3)";
 	try {
 		$db = getConnection(); 
 		$stmt = $db->prepare($sql);
-		$stmt->bindParam("usuario", $doc[0]->usuario);
-		$stmt->bindParam("password", $doc[0]->password);
-		$stmt->bindParam("nombre", $doc[0]->nombre);
-		$stmt->bindParam("servicios", $doc[0]->servicios);
-		$stmt->bindParam("telefono", $doc[0]->telefono);
-		$stmt->bindParam("horario", $doc[0]->horario);
-		$stmt->bindParam("direccion", $doc[0]->direccion);
-		$stmt->bindParam("correo", $doc[0]->correo);
-		$stmt->bindParam("foto1", $doc[0]->foto1);
-		$stmt->bindParam("foto2", $doc[0]->foto2);
-		$stmt->bindParam("foto3", $doc[0]->foto3);
+		$stmt->bindParam("usuario", $doc->usuario);
+		$stmt->bindParam("password", $doc->password);
+		$stmt->bindParam("nombre", $doc->nombre);
+		$stmt->bindParam("servicios", $doc->servicios);
+		$stmt->bindParam("telefono", $doc->telefono);
+		$stmt->bindParam("direccion", $doc->direccion);
+		$stmt->bindParam("correo", $doc->correo);
+		$stmt->bindParam("foto1", $doc->foto1);
+		$stmt->bindParam("foto2", $doc->foto2);
+		$stmt->bindParam("foto3", $doc->foto3);
 		$stmt->execute();
 		$db = null;
-		echo json_encode($doc[0]);
+		echo json_encode($doc);
 	} catch(PDOException $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
 	}
